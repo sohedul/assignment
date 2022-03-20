2b8a4925cae74aff9a7384c7a6ef1f058f3c11d7
Md Sohedul Islam <101013272+sohedul@users.noreply.github.com>
2022-03-20 18:26:59 +0600

    Add files via upload

---
+++ forloop.php
@@ -0,0 +1,51 @@
+<?php
+
+
+for($i=5000; $i<=10000; $i++){
+
+   if($i>6000 && $i<=6500){
+
+      if($i % 3==0 && $i % 4==0){
+
+         echo  " <h3 style= \"color:green;\"> I love PHP {$i} <br> </h3>";
+
+      }
+   }
+
+}
+
+
+
+/* decrement (500 round)
+
+for($i=5000; $i>=100; $i--){
+
+   if($i>100 && $i<=600){
+
+      if($i % 3==0 && $i % 4==0){
+
+         echo  " <h3 style= \"color:green;\"> I love PHP {$i} <br> </h3>";
+
+      }
+   }
+
+}
+
+
+
+
+/*
+for($i=5000; $i>=100; $i--){
+
+   if($i>100 && $i<=700){
+
+      if($i % 9==0){
+
+         echo  " <h3 style= \"color:green;\"> I love PHP {$i} <br> </h3>";
+
+      }
+   }
+
+}
+*/
+?>
