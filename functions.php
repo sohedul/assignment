17bc22e752eaffbddb5f8056d942c386775f3e64
Md Sohedul Islam <101013272+sohedul@users.noreply.github.com>
2022-03-20 18:25:13 +0600

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?
+
+/*
+
+function ageCal (string $name, int $year){
+
+    $age = 2022 - $year;
+
+echo "Hi {$name}, You are {$year} years old" ;
+
+
+}
+
+
+
+function boroHat($txt) {
+
+    echo "<span style = 'text-transform : uppercase;'>{$txt}</span>" ;
+
+}
+
+*/
+
+function chotoHat($txt) {
+
+    return "<span style = 'text-transform : lowercase;'> {$txt} </span>" ;
+
+}
+
+
+?>
+
